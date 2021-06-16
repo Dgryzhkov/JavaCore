e580472c32b323e2923f001363e1e03bfeed357a
Dgryzhkov <dgryzhkov@gmail.com>
2021-06-17 00:10:36 +0700

    less3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,5 @@
 public class HelloWorld {
-    public static void main (String [] args){
+    public static void main (String[] args){
         System.out.println("Hello World");
-        int myInt = 1;
-        short myShort=32000;
-        long myLong = 262453456;
-        double myDouble =1.000;
-        float myFloat = 1.0005f;
-        char c = 'c';
-        boolean d = true;
-        byte myByte = 8;
-        System.out.println(myInt);
     }
 }
