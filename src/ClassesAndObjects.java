cf12676272698c6b9bd03a2e77b263f3eb62a2ac
Dgryzhkov <dgryzhkov@gmail.com>
2021-06-22 22:51:02 +0700

    less18

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,32 +1,43 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person = new Person();
-        person.setNameAndAge("Vasya",50);
-    //    person.age = 55;
-        Person person2 = new Person();
-        String s="Kolya";
-        person2.setNameAndAge(s,20);
-   //     person2.age = 20;
+        person.setName("Petr");
+        person.setAge(-15);
+        System.out.println("Выводим значение в main методе " + person.getName());
+        System.out.println("Выводим значение в main методе " + person.getAge());
         person.speak();
-        person2.speak();
-        person2.sayHello();
-        System.out.println();
-        int year1 = person.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Первому человеку до пенсии: "+year1+" лет");
-        System.out.println("Второму человеку до пенсии: "+year2+" лет");
     }
 }
 
 class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
 
-
-    void setNameAndAge(String username, int userAge){
-        name=username;
-        age=userAge;
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        if (name.isEmpty()) {
+            System.out.println("Ты ввел пустое имя");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age<0) {
+            System.out.println("Возраст должен быть полжительный");
+        } else {
+            this.age = age;
+        }
+    }
+
+
     int calculateYearsToRetirement() {
         int years = 65 - age;
         //System.out.println("Колчиество лет до пенсии: "+years);
