f56a15ad7a4b63071b73d9814f823bf325f7c47d
Dgryzhkov <dgryzhkov@gmail.com>
2021-06-22 20:43:59 +0700

    less16

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -6,19 +6,31 @@ public class ClassesAndObjects {
         Person person2 = new Person();
         person2.name = "Vova";
         person2.age = 20;
-person.speak();
-person2.sayHello();
+        person.speak();
+        person2.sayHello();
+        System.out.println();
+        int year1 = person.calculateYearsToRetirement();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Первому человеку до пенсии: "+year1+" лет");
+        System.out.println("Второму человеку до пенсии: "+year2+" лет");
     }
 }
 
 class Person {
     String name;
     int age;
-    void speak(){
-        System.out.println("Меня зовут " +name+", мне "+age+" лет");
+
+    int calculateYearsToRetirement() {
+        int years = 65 - age;
+        //System.out.println("Колчиество лет до пенсии: "+years);
+        return years;
     }
 
-    void sayHello(){
+    void speak() {
+        System.out.println("Меня зовут " + name + ", мне " + age + " лет");
+    }
+
+    void sayHello() {
         System.out.println("Привет!");
     }
 }
