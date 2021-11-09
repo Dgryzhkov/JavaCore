02ee8fbce0e6a468443398a2016432ab4d4aaad1
Home <dgryzhkov@gmail.com>
2021-11-09 23:02:54 +0700

    less23Part2

--- src/lesson23.java
+++ src/lesson23.java
@@ -4,5 +4,7 @@ public class lesson23 {
         System.out.println(sb.toString());
         sb.append(" my").append(" friend");
         System.out.println(sb.toString());
+        System.out.printf("This is a string %10d \n",5);
+        System.out.printf("This is a string %.2f",5.12745687);
     }
 }
