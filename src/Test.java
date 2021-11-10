66e736f7dad97b17f00bc35409e00cb59e25fc24
Home <dgryzhkov@gmail.com>
2021-11-10 22:23:48 +0700

    lesson30

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        test(animal);
-        test(cat);
-        test(dog);
-
-
-    }
-
-    public static void test(Animal animal) {
-        animal.eat();
+        byte x = (byte) 260;
+        System.out.println(x);
     }
 }
