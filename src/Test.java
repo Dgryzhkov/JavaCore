aaf232bc5b9230e338cca3b6b77182f34606dd79
Home <dgryzhkov@gmail.com>
2021-11-11 09:55:25 +0700

    lesson35

--- src/Test.java
+++ src/Test.java
@@ -1,22 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
+interface AbleToEat {
+    public void eat();
+}
 
 public class Test {
     public static void main(String[] args) {
-        List<Animal> listOfAnimals = new ArrayList<>();
-        listOfAnimals.add(new Animal(1));
-        listOfAnimals.add(new Animal(2));
+        AbleToEat ableToEat = new AbleToEat() {
+            @Override
+            public void eat() {
+                System.out.println("Nyam Nyam");
+            }
 
-        List<Dog> listofDogs = new ArrayList<>();
-        listofDogs.add(new Dog());
-        listofDogs.add(new Dog());
-        test(listOfAnimals);
-        test(listofDogs);
-    }
 
-    private static void test(List<? extends Animal>list){
-        for (Animal animal: list) {
-           animal.eat();
-        }
+        };
+        ableToEat.eat();
     }
 }
