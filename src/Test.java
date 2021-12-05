e3b5db76c23a1bed50c1ec35edc0f1292dfa0b45
Home <dgryzhkov@gmail.com>
2021-12-05 23:20:50 +0700

    lesson49

--- src/Test.java
+++ src/Test.java
@@ -1,43 +1,27 @@
 public class Test {
 
     public static void main(String[] args) {
-        Season season= Season.SUMMER;
-        Animal animal = Animal.CAT;
-        System.out.println(season.name());
-        System.out.println(animal.name());
-
-        Animal frog = Animal.valueOf("FROG");
-        System.out.println(frog.getTranslation());
-
-
-        Season winter = Season.WINTER;
-        System.out.println(winter.ordinal());
-//        Animal animal = Animal.CAT;
-//        System.out.println(animal);
+//        counter(3);
 //
-//        switch (animal) {
-//            case CAT:
-//                System.out.println("CAT");
-//                break;
-//            case DOG:
-//                System.out.println("DOD");
-//                break;
-//        }
-
-        // Object->Enum->Season
+//    }
+//
+//    private static void counter(int n) {
+//        if (n==0)
+//            return;
+//
+//        System.out.println(n);
+//
+//        counter(n - 1);
 //
 //
-//        Season season = Season.SUMMER;
-//        System.out.println(season.getTemperature());
-//        System.out.println(season instanceof Enum);
-//        System.out.println(season);
-//
-//        switch (season) {
-//            case SUMMER:
-//                System.out.println("Hot");
-//                break;
-//            case AUTUMN:
-//                System.out.println("green");
-//                break;
-        }
+//    }
+
+        System.out.println(fac(4));
+
     }
+    private static int fac ( int n){
+        if (n == 1)
+            return 1;
+        return n * fac(n-1);
+    }
+}
