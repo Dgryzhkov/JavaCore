cc6aa5def0b5e4424e8688867bf3bdfe3c24372a
Home <dgryzhkov@gmail.com>
2021-12-05 15:54:01 +0700

    less45

--- src/WriteObject.java
+++ src/WriteObject.java
@@ -7,12 +7,14 @@ public class WriteObject {
     public static void main(String[] args) {
         Person person1 = new Person(1, "Bob");
         Person person2 = new Person(2,"Mike");
+        Person person3 = new Person(3,"Mary");
 
         try {
             FileOutputStream fos = new FileOutputStream("people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(person1);
             oos.writeObject(person2);
+            oos.writeObject(person3);
             oos.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not Found");
